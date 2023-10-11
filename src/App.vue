8cbdd52f7bdf5c39b18667ed380e6429e086bd4a
eserengo <eserengo@gmail.com>
2023-10-11 11:53:40 -0300

    Added router, views and navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,62 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      media: { Boolean },
+      isActive: false,
+    }
+  },
+
+  methods: {
+    setMedia() {
+      let timer;
+      window.clearTimeout(timer);
+      timer = window.setTimeout(
+        window.matchMedia("(width<=576px)").matches
+          ? this.media = true
+          : this.media = false
+        , 500);
+    },
+    toggleIsActive() {
+      this.isActive = !this.isActive;
+    },
+  },
+
+  mounted() {
+    this.setMedia();
+    window.addEventListener("resize", () => this.setMedia());
+  },
+  unmounted() {
+    window.removeEventListener("resize", () => this.setMedia());
+  },
+}
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/destination">Destination</RouterLink>
-      <RouterLink to="/crew">Crew</RouterLink>
-      <RouterLink to="/technology">Technology</RouterLink>
+    <img src="./assets/shared/logo.svg" alt="logo" />
+
+    <nav v-if="media">
+      <section v-if="isActive">
+        <img src="./assets/shared/icon-close.svg" alt="close icon" @click="toggleIsActive"/>
+        <router-link to="/" @click="toggleIsActive">01 Home</router-link> |
+        <router-link to="/destination" @click="toggleIsActive">02 Destination</router-link> |
+        <router-link to="/crew" @click="toggleIsActive">03 Crew</router-link> |
+        <router-link to="/technology" @click="toggleIsActive">04 Technology</router-link>
+      </section>
+      <img src="./assets/shared/icon-hamburger.svg" alt="hamburger icon" v-else @click="toggleIsActive"/>
+    </nav>
+
+    <nav v-else>
+      <router-link to="/">01 Home</router-link> |
+      <router-link to="/destination">02 Destination</router-link> |
+      <router-link to="/crew">03 Crew</router-link> |
+      <router-link to="/technology">04 Technology</router-link>
     </nav>
   </header>
 
-  <RouterView />
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView
     },
     {
-      path: '/destination',
-      name: 'destination',
-      component: () => import('../views/DestinationView.vue')
+      path: "/destination",
+      name: "destination",
+      component: () => import("../views/DestinationView.vue")
     },
     {
-      path: '/crew',
-      name: 'crew',
-      component: () => import('../views/CrewView.vue')
+      path: "/crew",
+      name: "crew",
+      component: () => import("../views/CrewView.vue")
     },
     {
-      path: '/technology',
-      name: 'technology',
-      component: () => import('../views/TechnologyView.vue')
+      path: "/technology",
+      name: "technology",
+      component: () => import("../views/TechnologyView.vue")
     },
     
     // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
+    // this generates a separate chunk (...[hash].js) for this route
     // which is lazy-loaded when the route is visited.
   ]
 })
